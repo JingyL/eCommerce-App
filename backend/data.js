17ad338887cdc9889ff8a1b5830e43cb7af06db5
Jingyi Lin <linjingyikayla@gmail.com>
2022-02-20 02:15:23 -0500

    add loading and error msgs

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Sulwhasoo Concentrate',
+            category: 'Treatment',
+            image: '/images/Sulwhasoo-concen.jpg',
+            price: 120,
+            countInStock:500,
+            brand: 'Sulwhasoo',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'skin product'
+        },
+        {
+            _id: '2',
+            name: 'Sulwhasoo Cream',
+            category: 'Cream',
+            image: '/images/Sulwhasoo-cream.jpg',
+            price: 150,
+            countInStock:500,
+            brand: 'Sulwhasoo',
+            rating: 4.7,
+            numReviews: 100,
+            description: 'Skin product'
+        },
+        {
+            _id: '3',
+            name: 'Sulwhasoo Mask',
+            category: 'Mask',
+            image: '/images/Sulwhasoo-mask.jpg',
+            price: 75,
+            countInStock:500,
+            brand: 'Sulwhasoo',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Skin product'
+        },
+        {
+            _id: '4',
+            name: 'Laneige Base',
+            category: 'MakeUp',
+            image: '/images/Laneige-base.jpg',
+            price: 35,
+            countInStock:500,
+            brand: 'Laneige',
+            rating: 4.5,
+            numReviews: 130,
+            description: 'MakeUp product'
+        },
+        {
+            _id: '5',
+            name: 'Laneige Corrector',
+            category: 'MakeUp',
+            image: '/images/Laneige-corrector.jpg',
+            price: 35,
+            countInStock:500,
+            brand: 'Laneige',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'MakeUp product'
+        },
+        {
+            _id: '6',
+            name: 'Sulwhasoo Set',
+            category: 'Gift Set',
+            image: '/images/Laneige-set.jpg',
+            price: 120,
+            countInStock:500,
+            brand: 'Laneige',
+            rating: 4.5,
+            numReviews: 50,
+            description: 'skin product'
+        }
+    ]
+}
+
+export default data;
